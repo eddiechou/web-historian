25455b57c81473467405b463a25844b5ae6220a1
Eddie Chou <eddieechou@gmail.com>
2017-03-05 18:11:08 -0800

    Code clean up

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -44,14 +44,6 @@ exports.initialize = function(pathsObj) {
 
 exports.readListOfUrls = function(callback) {
   fs.readFile(exports.paths.list, function(err, data) {
-    // console.log('///data in readfile:', (data)); 
-    // console.log('in readListOfUrls');
-    // console.log('// obj:', data.toString());
-    // console.log('//Sites: ', sites);
-    // console.log('***is sites an array', Array.isArray(sites));
-    // data should contain array of sites
-    // callback(obj);
-
     var wholeString = data.toString();
     var sites = wholeString.split('\n');
     if (callback) {
@@ -61,17 +53,6 @@ exports.readListOfUrls = function(callback) {
 };
 
 exports.isUrlInList = function(url, callback) {
-  //call readURL
-  // console.log('CHECKING FOR READLIST', exports.readListOfUrls);
-  // exports.readListOfUrls(function(sites) {
-  //   //check for target URL in readURL array
-  //   // console.log('// IN isURLInList callback');
-  //   // console.log('// getting sites inside:', sites);
-  //   // console.log('//Within URLINLIST:', result);
-   
-  //   var result = sites.indexOf( url) > -1;   
-  //   callback(result);
-  // });
   exports.readListOfUrls(function(sites) {
     var found = _.any(sites, function(site, i) {
       return site.match(url);
@@ -83,20 +64,6 @@ exports.isUrlInList = function(url, callback) {
 
 // Should append as strings with \n
 exports.addUrlToList = function(url, callback) {
-  //Call readListOfUrls
-  // exports.readListOfUrls(function(sites) {
-    //Add to the result array
-    // console.log('///SITES WITHIN URL:', sites);
-  //   sites.push(url);
-  //   //Split the results by newline
-
-  //   var results = sites.join('\n');
-  //   console.log('......///', results);
-  //   // console.log('****/// sites within;', sites);
-  //   fs.writeFile(exports.paths.list, results, callback);
-  //   // console.log('///Within addToUrl exports list*** is:', exports.path.list);
-  //   //Write the result back to the file
-  // });
   fs.appendFile(exports.paths.list, url + '\n', function(err, file) {
     callback();
   });
@@ -104,60 +71,18 @@ exports.addUrlToList = function(url, callback) {
 
 exports.isUrlArchived = function(url, callback) {
   // Check if fie with url as name exists in archivedSites
-  // console.log('URL');
-
   var testUrl = exports.paths.archivedSites + '/' + url;
   fs.exists(testUrl, function(exists) {
     callback(exists);
   });
 };
 
-// Input: URL array
-// Output: nothing
 exports.downloadUrls = function(urls) {
   // For each URL in urls
   urls.forEach(function(url) {
     if (!url) { return; }
     request('http://' + url).pipe(fs.createWriteStream(exports.paths.archivedSites + '/' + url));
   });
-    // exports.isUrlArchived(url, function(result) {
-    //   // If not archived
-    //   if (!result) {
-    //     // Go get the file (HTML, CSS)
-    //     var options = {
-    //       host: url,
-    //       post: 80,
-    //       path: ''
-    //     };
-    //     http.get(options, function(res) {
-    //       var html = '';
-    //       res.on('data', function(chunk) {
-    //         html += chunk;
-    //       });
-    //       res.on('end', function() {
-    //         // var hostName = urlParse.parse(url).hostname;
-    //         // console.log('//////////URL: ', url);
-    //         // console.log('//////////HOstnAMAE: ', hostName);
-    //         //url = www.google.com
-    //         // console.log('&&&&&& URL BEFORE:', url);
-    //         url = url.replace('www.', '').replace('.com', '');
-    //         // console.log('&&&&&& URL AFTER:', url);
-    //         if (!fs.existsSync(exports.paths.archivedSites + '/' + url)) {
-    //           fs.mkdirSync(exports.paths.archivedSites + '/' + url);
-    //         }
-    //         fs.writeFileSync(exports.paths.archivedSites + '/' + url + '/index.html', html);
-    //       });
-    //       // console.log('Got results', res);
-    //     }).on('error', function(e) {
-    //       console.log('Got error:' + e.message);
-    //     });
-    //     // Create the archive directory
-    //     // Put files in archive directory
-          
-    //   }
-    // });
-
-// })
 };
 
 
